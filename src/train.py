5841261f9e3e3aa016de33a7d28c22baeec527b8
Niklas Henning <niklashenningbusiness@gmail.com>
2024-10-07 11:43:25 +0200

    Add model class

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+import torch
+import torch.nn as nn
 import pandas as pd
 from torchvision import transforms
 from torch.utils.data import Dataset
@@ -21,3 +23,17 @@ class HwD1000Dataset(Dataset):
         image = self.transform(Image.open(image_path).convert('L'))
         label = self.dataframe.iloc[idx, 1]
         return image, label
+
+
+class Model(nn.Module):
+
+    def __init__(self):
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
